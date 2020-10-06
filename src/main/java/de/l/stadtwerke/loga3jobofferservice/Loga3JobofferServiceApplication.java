f971c6a54c2505b8075bdd3cc0b129a6f8bbf61c
Karim <karim.rakia@adesso.de>
2020-10-06 11:49:41 +0200

    first commit

---
+++ src/main/java/de/l/stadtwerke/loga3jobofferservice/Loga3JobofferServiceApplication.java
@@ -0,0 +1,13 @@
+package de.l.stadtwerke.loga3jobofferservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Loga3JobofferServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Loga3JobofferServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/l/stadtwerke/loga3jobofferservice/Loga3JobofferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.l.stadtwerke.loga3jobofferservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Loga3JobofferServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
