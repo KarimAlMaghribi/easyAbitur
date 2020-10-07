a44c0433ade6cba47e634b479890f73a6ac547d6
Karim <karim.rakia@adesso.de>
2020-10-07 12:39:31 +0200

    creation of Stellenausschreibung-Entity and StellenausschreibungDTO's

---
+++ src/main/java/de/l/stadtwerke/loga3jobofferservice/dto/StellenausschreibungDTO.java
@@ -0,0 +1,4 @@
+package de.l.stadtwerke.loga3jobofferservice.dto;
+
+public class StellenausschreibungDTO {
+}
---
+++ src/main/java/de/l/stadtwerke/loga3jobofferservice/model/Stellenausschreibung.java
@@ -0,0 +1,44 @@
+package de.l.stadtwerke.loga3jobofferservice.model;
+
+/*
+* Titel	Feld title
+Freitext (optional)	(Haken)
+Liste mit Aufgaben "Ihre Aufgaben"	(Haken)
+Angebote seitens des jeweiligen BU	(Fehler) (sind die ggf. nicht immer vorhanden?)
+Profil des Bewerbers - "
+Auf diese Aufgaben sind Sie gut vorbereitet, wenn Sie folgende Voraussetzungen
+
+erfüllen"
+
+(Haken)
+Footer des jeweiligen BU	(Fehler)
+Short Note "Gut zu wissen"	(Fehler) (sind die ggf. nicht immer vorhanden?)
+Ansprechpartner	(Haken) (schwer zu parsen)
+Link zu Bewerbungsportal des jeweiligen BU
+*
+* */
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.util.Set;
+
+/**
+ * Created by Arpit Khandelwal.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+//@Accessors(chain = true)
+@Document(collection = "stellenausschreibung")
+public class Stellenausschreibung {
+}
