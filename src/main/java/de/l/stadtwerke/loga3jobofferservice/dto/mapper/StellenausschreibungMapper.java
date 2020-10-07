20fe78d2c238ee77afe798d86a3b74200b3a562b
Karim <karim.rakia@adesso.de>
2020-10-07 12:38:51 +0200

    creation of mapping between Entities and DTO's

---
+++ src/main/java/de/l/stadtwerke/loga3jobofferservice/dto/mapper/StellenausschreibungMapper.java
@@ -0,0 +1,11 @@
+package de.l.stadtwerke.loga3jobofferservice.dto.mapper;
+
+import de.l.stadtwerke.loga3jobofferservice.dto.StellenausschreibungDTO;
+import de.l.stadtwerke.loga3jobofferservice.model.Stellenausschreibung;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface StellenausschreibungMapper {
+    Stellenausschreibung mapToStellenausschreibung(StellenausschreibungDTO stellenausschreibungDTO);
+    StellenausschreibungDTO mapStellenausschreibungToDTO(Stellenausschreibung stellenausschreibung);
+}
